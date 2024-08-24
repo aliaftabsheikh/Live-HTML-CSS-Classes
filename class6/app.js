3d3e4d64df1911addea1ae65daaeb3dd5bd02cea
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2024-08-25 01:07:28 +0500

    class-7

---
+++ class6/app.js
@@ -0,0 +1,31 @@
+// let fruits = ["Apple", "Banana", "Cherry", "Date"];
+// let removed = fruits.splice(1, 3);
+// console.log(fruits); // Output: ["Apple", "Date"]
+// console.log(removed); // Output: ["Banana", "Cherry"]
+
+// let fruits = ["Apple", "Date"];
+// fruits.splice(1, 1, "Banana", "Cherry");
+// console.log(fruits); // Output: ["Apple", "Banana", "Cherry", "Date"]
+
+// let fruits = ["Apple", "Banana", "Cherry", "Date"];
+// fruits.splice(2);
+// console.log(fruits); // Output: ["Apple", "Banana"]
+
+// let fruits = ["Apple", "Banana", "Cherry", "Date"];
+// let slicedFruits = fruits.slice(1, 2);
+// console.log(slicedFruits); // Output: ["Banana", "Cherry"]
+// console.log(fruits); // Output: ["Apple", "Banana", "Cherry", "Date"]
+
+// let fruits = ["Apple", "Banana", "Cherry", "Date"];
+// let slicedFruits = fruits.slice(0, fruits.length -1
+// );
+// console.log(slicedFruits); // Output: ["Banana", "Cherry"]
+
+
+let text = "Hello, World!";
+let slicedText = text.slice(7, 12);
+console.log(slicedText); // Output: "World"
+
+
+
+
